dc1128d90f48ad0dea679f79296f944030fa9dac
mohammad.zolfaghari <zolfaghary71@gmail.com>
2023-12-28 20:53:49 +0330

    added queue command for videos

---
+++ Part.VideoUploader.Service/Features/UploadQueue/UploadQueueCommand.cs
@@ -0,0 +1,10 @@
+﻿namespace Part.VideoUploader.Service.Features.UploadQueue;
+
+public class UploadQueueCommand
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public string Path { get; set; }
+    public DateTime DateCreated { get; set; }
+}
