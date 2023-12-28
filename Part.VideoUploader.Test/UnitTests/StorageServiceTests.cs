1a06f1484c773f4a7561d447ab34115545d69333
mohammad.zolfaghari <zolfaghary71@gmail.com>
2023-12-28 16:32:41 +0330

    add Failing Test for Saving to storge service

---
+++ Part.VideoUploader.Test/UnitTests/StorageServiceTests.cs
@@ -0,0 +1,21 @@
+﻿using Moq;
+using Xunit;
+
+namespace Part.VideoUploader.Test.UnitTests;
+
+public class StorageServiceTests
+{
+    [Fact]
+    public async Task SaveToStorage()
+    {
+        var mockMinioClient = new Mock<IStorageWrapper>();
+        var service = new StorageService(mockMinioClient.Object, "mybucket");
+
+        string testFilePath = "testfile.mp4";
+        string expectedObjectName = "testfile.mp4"; 
+
+        await service.UploadFileAsync(testFilePath, expectedObjectName);
+
+        mockMinioClient.Verify(client => client.PutObjectAsync("mybucket", expectedObjectName, testFilePath, "video/mp4"), Times.Once());
+    }
+}
