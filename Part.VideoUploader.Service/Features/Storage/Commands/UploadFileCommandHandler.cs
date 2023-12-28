01b4850f4258c9ff3455df366dae432ddc3895a7
mohammad.zolfaghari <zolfaghary71@gmail.com>
2023-12-28 17:42:27 +0330

    add UploadFileCommandHandler.cs

---
+++ Part.VideoUploader.Service/Features/Storage/Commands/UploadFileCommandHandler.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Part.VideoUploader.Service.Features.Storage.Commands;
+
+public class UploadFileCommandHandler: IRequestHandler<UploadFileCommand, UploadFileCommandResponse>
+{
+    public Task<UploadFileCommandResponse> Handle(UploadFileCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
