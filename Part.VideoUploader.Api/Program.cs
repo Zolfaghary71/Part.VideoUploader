7c7b6905d2e0e5e294ca56ea047e646e4ad55c09
mohammad.zolfaghari <zolfaghary71@gmail.com>
2023-12-29 15:03:22 +0330

    register DI for 2 services

--- Part.VideoUploader.Api/Program.cs
+++ Part.VideoUploader.Api/Program.cs
@@ -1,5 +1,9 @@
 using MediatR;
+using Part.VideoUploader.Infrastructure.RabbitMq;
+using Part.VideoUploader.Infrastructure.Storage;
+using Part.VideoUploader.Service.Contracts.Infrastructure;
 using Part.VideoUploader.Service.Features.Storage.Commands;
+using Part.VideoUploader.Service.Features.UploadQueue.Command;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +14,9 @@ builder.Services.AddStackExchangeRedisCache(options =>
     options.Configuration = "localhost"; 
     options.InstanceName = "SampleInstance";
 });
+
+builder.Services.AddTransient<IQueue<EnqueueVideoUploadCommand>, RabbitMqQueue<EnqueueVideoUploadCommand>>();
+builder.Services.AddTransient<IStorageService, StorageService>();
 
 var app = builder.Build();
 
