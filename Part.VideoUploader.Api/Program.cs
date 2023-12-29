7b4c0386b8e6b0f42d4c0c7fafb53a6aca057451
mohammad.zolfaghari <zolfaghary71@gmail.com>
2023-12-29 12:22:04 +0330

    add redis repo

--- Part.VideoUploader.Api/Program.cs
+++ Part.VideoUploader.Api/Program.cs
@@ -2,8 +2,15 @@ using MediatR;
 using Part.VideoUploader.Service.Features.Storage.Commands;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddMediatR(typeof(UploadFileCommand).Assembly);
-var app = builder.Build();
 
+builder.Services.AddMediatR(typeof(UploadFileCommand).Assembly);
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = "localhost"; 
+    options.InstanceName = "SampleInstance";
+});
+
+var app = builder.Build();
 
 app.Run();
---
+++ Part.VideoUploader.Infrastructure/Redis/RedisRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Part.VideoUploader.Domain;
+using Part.VideoUploader.Service.Contracts.Infrastructure;
+using System;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Part.VideoUploader.Infrastructure.Redis
+{
+    public class RedisRepository : IRedisRepository<VideoFile>
+    {
+        private readonly IDistributedCache _cache;
+
+        public RedisRepository(IDistributedCache cache)
+        {
+            _cache = cache;
+        }
+
+        public async Task<VideoFile> Get(Guid id)
+        {
+            var cachedData = await _cache.GetAsync(id.ToString());
+            if (cachedData == null)
+            {
+                return null;
+            }
+
+            return JsonSerializer.Deserialize<VideoFile>(cachedData);
+        }
+
+        public async Task<bool> Set(VideoFile videoFile)
+        {
+            var jsonData = JsonSerializer.SerializeToUtf8Bytes(videoFile);
+            var options = new DistributedCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromHours(24)
+            };
+
+            await _cache.SetAsync(videoFile.Id.ToString(), jsonData, options);
+            return true;
+        }
+
+        public async Task<bool> Delete(string id)
+        {
+            await _cache.RemoveAsync(id);
+            return true;
+        }
+    }
+}
