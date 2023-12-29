75832639103e056d44731a73dbd35b81944eaa51
mohammad.zolfaghari <zolfaghary71@gmail.com>
2023-12-29 12:00:45 +0330

    add failing test for EnqueueCommandHandler

---
+++ Part.VideoUploader.Service/Contracts/Infrastructure/IRedisRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Part.VideoUploader.Service.Contracts.Infrastructure;
+
+public interface IRedisRepository<T>
+{
+    public Task<T> Get(Guid id);
+    public Task<bool> Set(T @object);
+    public Task<bool> Delete(string id);
+}
---
+++ Part.VideoUploader.Test/UnitTests/EnqueueCommandHandlerTest.cs
@@ -0,0 +1,33 @@
+﻿using Moq;
+using Xunit;
+using Part.VideoUploader.Domain;
+using Part.VideoUploader.Service.Contracts.Infrastructure;
+using Part.VideoUploader.Service.Features.UploadQueue;
+
+
+namespace Part.VideoUploader.Test.UnitTests;
+
+public class EnqueueCommandHandlerTest
+{
+    [Fact]
+    public async void EnqueueCommandHandlerTest_ShouldEndqueue()
+    {
+        var video = new VideoFile()
+        {
+            Id = Guid.NewGuid(),
+            Name = "videoName",
+            Path = "videoPath",
+            Type = "mp4",
+            DateCreated = DateTime.Now
+        };
+
+        var redisRepo = new Mock<IRedisRepository<VideoFile>>();
+
+        var commandHandler = new EnqueueCommandHandler();
+        redisRepo.Setup(r => r.Set(video)).ReturnsAsync(true);
+        
+        redisRepo.Verify(s => s.Set(video), Times.Once);
+
+
+    }
+}
