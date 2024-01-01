bcecaef3b9eef3acc7a5fe6c3f25f54afe779e1c
mohammad.zolfaghari <zolfaghary71@gmail.com>
2024-01-01 19:13:03 +0330

    add di injection to db context

--- Part.VideoUploader.Api/Program.cs
+++ Part.VideoUploader.Api/Program.cs
@@ -1,13 +1,18 @@
 using MediatR;
-using Part.VideoUploader.Infrastructure.RabbitMq;
+using Microsoft.EntityFrameworkCore;
+using Part.VideoUploader.Infrastructure.Db;
+using Part.VideoUploader.Infrastructure.Repository;
 using Part.VideoUploader.Infrastructure.Storage;
 using Part.VideoUploader.Service.Contracts.Infrastructure;
 using Part.VideoUploader.Service.Features.Storage.Commands;
-using Part.VideoUploader.Service.Features.UploadQueue.Command;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddMediatR(typeof(UploadFileCommand).Assembly);
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IFileUploadInfoRepository, FileUploadInfoRepository>();
 
 builder.Services.AddStackExchangeRedisCache(options =>
 {
@@ -15,7 +20,6 @@ builder.Services.AddStackExchangeRedisCache(options =>
     options.InstanceName = "SampleInstance";
 });
 
-builder.Services.AddTransient<IQueue<EnqueueVideoUploadCommand>, RabbitMqQueue<EnqueueVideoUploadCommand>>();
 builder.Services.AddTransient<IStorageService, StorageService>();
 
 var app = builder.Build();
