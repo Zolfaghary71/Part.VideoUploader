506a9751f666c086033aae86870c0d12ab5a80ff
mohammad.zolfaghari <zolfaghary71@gmail.com>
2024-01-01 18:10:51 +0330

    update UploadFileCommand.cs

---
+++ Part.VideoUploader.Api/Controllers/FileUploadController.cs
@@ -0,0 +1,39 @@
+﻿using System.Security.Claims;
+using MassTransit.Mediator;
+using Microsoft.AspNetCore.Mvc;
+using Part.VideoUploader.Service.Features.Upload.Commands;
+
+namespace Part.VideoUploader.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class FileUploadController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public FileUploadController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost("upload")]
+    public async Task<IActionResult> Upload()
+    {
+        var file = HttpContext.Request.Form.Files[0];
+        if (file.Length == 0)
+        {
+            return BadRequest("Empty file received.");
+        }
+
+        var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        var fileName = file.FileName;
+        var contentType = file.ContentType;
+
+        using (var stream = file.OpenReadStream())
+        {
+            var uploadId = await _mediator.Send(new UploadFileCommand(stream, fileName, contentType, userId));
+            return Ok(new { UploadId = uploadId });
+        }
+    }
+}
---
+++ Part.VideoUploader.Service/Features/Upload/Commands/UploadFileCommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+
+namespace Part.VideoUploader.Service.Features.Upload.Commands;
+
+public class UploadFileCommand : IRequest<Guid>
+{
+    public Stream FileStream { get; }
+    public string FileName { get; }
+    public string ContentType { get; }
+    public string UserId { get; }
+
+    public UploadFileCommand(Stream fileStream, string fileName, string contentType, string userId)
+    {
+        FileStream = fileStream;
+        FileName = fileName;
+        ContentType = contentType;
+        UserId = userId;
+    }
+}
