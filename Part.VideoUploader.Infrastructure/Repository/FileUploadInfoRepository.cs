686c5f2ca7f24718a2bd7ec94d58b374ebdb06a1
mohammad.zolfaghari <zolfaghary71@gmail.com>
2024-01-01 19:11:59 +0330

    add file upload info repository

---
+++ Part.VideoUploader.Infrastructure/Repository/FileUploadInfoRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Part.VideoUploader.Domain;
+using Part.VideoUploader.Infrastructure.Db;
+using Part.VideoUploader.Service.Contracts.Infrastructure;
+
+namespace Part.VideoUploader.Infrastructure.Repository;
+
+
+public class FileUploadInfoRepository : IFileUploadInfoRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public FileUploadInfoRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddAsync(FileUploadInfo fileUploadInfo)
+    {
+        await _context.FileUploadInfos.AddAsync(fileUploadInfo);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(FileUploadInfo fileUploadInfo)
+    {
+        _context.FileUploadInfos.Update(fileUploadInfo);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<IEnumerable<FileUploadInfo>> GetFileUploadsByUserAsync(string userId)
+    {
+        return await _context.FileUploadInfos
+            .Where(f => f.UserId == userId)
+            .ToListAsync();
+    }
+}
